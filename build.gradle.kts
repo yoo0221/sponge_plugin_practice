bffce032551968606dc8aace913a1f633922f6e7
yoo0221 <niceyoo0221@knu.ac.kr>
2024-02-27 12:50:41 +0900

    Initial Commit: Sponge Project Created

---
+++ build.gradle.kts
@@ -0,0 +1,62 @@
+import org.spongepowered.gradle.plugin.config.PluginLoaders
+import org.spongepowered.plugin.metadata.model.PluginDependency
+
+plugins {
+    `java-library`
+    id("org.spongepowered.gradle.plugin") version "2.1.1"
+}
+
+group = "org.test"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+
+}
+
+sponge {
+    apiVersion("10.0.0")
+    license("MIT")
+    loader {
+        name(PluginLoaders.JAVA_PLAIN)
+        version("1.0")
+    }
+    plugin("test") {
+        displayName("test")
+        entrypoint("org.test.test.Untitled")
+        description("My plugin description")
+        links {
+            // homepage("https://spongepowered.org")
+            // source("https://spongepowered.org/source")
+            // issues("https://spongepowered.org/issues")
+        }
+        dependency("spongeapi") {
+            loadOrder(PluginDependency.LoadOrder.AFTER)
+            optional(false)
+        }
+    }
+}
+
+val javaTarget = 17 // Sponge targets a minimum of Java 17
+java {
+    sourceCompatibility = JavaVersion.toVersion(javaTarget)
+    targetCompatibility = JavaVersion.toVersion(javaTarget)
+    if (JavaVersion.current() < JavaVersion.toVersion(javaTarget)) {
+        toolchain.languageVersion.set(JavaLanguageVersion.of(javaTarget))
+    }
+}
+
+tasks.withType(JavaCompile::class).configureEach {
+    options.apply {
+        encoding = "utf-8" // Consistent source file encoding
+        if (JavaVersion.current().isJava10Compatible) {
+            release.set(javaTarget)
+        }
+    }
+}
+
+// Make sure all tasks which produce archives (jar, sources jar, javadoc jar, etc) produce more consistent output
+tasks.withType(AbstractArchiveTask::class).configureEach {
+    isReproducibleFileOrder = true
+    isPreserveFileTimestamps = false
+}
